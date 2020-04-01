e3a60f837d1629c451c95b4b014b50e874142d72
FaridLU <faridstudylu@gmail.com>
2020-04-02 03:58:25 +0600

    Added color output along with this removed 'migrate' command from script

--- automated_django_script.sh
+++ automated_django_script.sh
@@ -1,45 +1,47 @@
 project_name=$1
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NC=$(tput sgr0) #No color
 
 if [ -z "$project_name" ]; then
-	echo ERROR: Project Name is not provided
+	echo ERROR: ${RED}Project Name is not provided
 	exit 0
 fi
 
-
-echo '*** Install required package of pip *** '
+echo "***${GREEN} Install required package of pip ${NC}***"
 apt install python3-pip
 pip3 install virtualenv 
 
-echo '*** Installing Django globally ***'
+echo "***${GREEN} Installing Django globally ${NC}***"
 pip3 install Django
 
-echo '*** Create Django Project ***'
+echo "***${GREEN} Create Django Project ${NC}***"
 mkdir ~/Desktop/Django\ Projects/$project_name
 django-admin startproject $project_name ~/Desktop/Django\ Projects/$project_name && sleep 3
 
 
-echo '*** Setup "static" file settings  ***'
+echo "***${GREEN} Setup "static" file settings  ${NC}***"
 mkdir ~/Desktop/Django\ Projects/$project_name/static
 cp -r MDBootstrap/* ~/Desktop/Django\ Projects/$project_name/static
 cat extra_settings.txt >> ~/Desktop/Django\ Projects/$project_name/$project_name/settings.py
 
-echo '*** Make templates directory ***'
+echo "***${GREEN} Make templates directory ${NC}***"
 mkdir ~/Desktop/Django\ Projects/templates
 cp base.html ~/Desktop/Django\ Projects/templates
 
-echo '*** Update templates directory ***'
-sed -i "s/'DIRS':\ \[],/'DIRS':\ \[os.path.join(BASE_DIR, 'templates')],/g" ~/Desktop/Django\ Projects/farid/farid/settings.py
+echo "***${GREEN} Update templates directory ${NC}***"
+sed -i "s/"DIRS":\ \[],/"DIRS":\ \[os.path.join(BASE_DIR, "templates")],/g" ~/Desktop/Django\ Projects/farid/farid/settings.py
 
-echo '*** Go to project directory ***'
+echo "***${GREEN} Go to project directory ${NC}***"
 cd ~/Desktop/Django\ Projects/$project_name
 
-echo '*** Create virtualenv ***'
+echo "***${GREEN} Create virtualenv ${NC}***"
 python3 -m venv venv
 
-echo '*** Activate virtualenv ***'
+echo "***${GREEN} Activate virtualenv ${NC}***"
 source venv/bin/activate && sleep 2
 
-echo '*** Install esential packages of python ***'
+echo "***${GREEN} Install esential packages of python ${NC}***"
 pip install Django 					# For running Django projects
 pip install django-crispy-forms		# For using good looking forms
 pip install djangorestframework		# For using Django Rest Framework
@@ -48,12 +50,12 @@ pip install django-filter  			# Filtering support
 
 pip freeze > requirements.txt
 
-echo '*** Making migrations ***'
+echo "***${GREEN} Making migrations ${NC}***"
 python3 manage.py makemigrations && sleep 5
-python3 manage.py migrate && sleep 5
+# python3 manage.py migrate && sleep 5
 
 python3 manage.py createsuperuser
 
 python3 manage.py runserver 0:8000
 
-echo 'Done'
+echo "${NC}Done"
